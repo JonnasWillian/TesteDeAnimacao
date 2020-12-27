9f99032a9b6392e69a8e68646a0d45ddd2e3ebf0
JonnasWillian <jonnasnogueira2@gmail.com>
2020-12-27 08:41:22 -0300

    'TesteParaAnimacao'

---
+++ imc/script.js
@@ -0,0 +1,5 @@
+let alt = document.querySelector("input#alt")
+let peso = document.querySelector("select#peso")
+let peso = document.querySelector("select#idade")
+let ida = document.querySelector("div#res")
+let valores = []
