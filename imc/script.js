470ddd4d68cc50e5a13b2004f2c567684277e684
JonnasWillian <jonnasnogueira2@gmail.com>
2021-01-07 22:04:26 -0300

    'AtualizacaoDaCalculadoraImc'

--- imc/script.js
+++
@@ -1,7 +0,0 @@
-let alt = document.querySelector("input#alt")
-let peso = document.querySelector("select#peso")
-let peso = document.querySelector("select#idade")
-let ida = document.querySelector("div#res")
-let valores = []
-
-function soma(a,p,)
